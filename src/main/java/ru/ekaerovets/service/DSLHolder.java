8cd66e7f9643026a3bb78f44b7764ad793e373cd
dmitry.karyakin <dmitry.karyakin>
2016-09-29 20:08:11 +0300

    First working prototype

--- src/main/java/ru/ekaerovets/service/DSLHolder.java
+++ src/main/java/ru/ekaerovets/service/DSLHolder.java
@@ -1,8 +1,15 @@
 package ru.ekaerovets.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.*;
+import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,26 +20,40 @@ import java.util.Map;
 @Component
 public class DSLHolder {
 
-    private static final String DSLFile = "/home/laby/proj/chinese_zi/dabkrs_v70_1/大БКРС_v70.dsl";
-
-    public static void main(String[] args) {
-        new DSLHolder();
-    }
-
+    @Value("${bkrs.path}")
+    private String DSLFile;
 
     private Map<String, Entry> entries;
 
+    public static void main(String[] args) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get("C:\\\\proj\\\\chzi\\\\dabkrs\\\\bkrs_v71.dsl"));
+        boolean skip = true;
+        for (int i = 0; i < 100000000; i++) {
+            if (bytes[i] == 53 && bytes[i + 1] == 85) {
+                if (skip) {
+                    skip = false;
+                    continue;
+                }
+                for (int j = 0; j < 30; j++) {
+                    System.out.print(Integer.toHexString(0xFF & bytes[i + j]) + " ");
+                }
+            }
+        }
+
+    }
+
     public DSLHolder() {
 
-//        entries = new HashMap<>();
+    }
+
+    @PostConstruct
+    public void init() {
         try {
             entries = readDSL(DSLFile);
             System.out.println(entries.size());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 
     private Map<String, Entry> readDSL(String filename) throws IOException {
@@ -47,19 +68,28 @@ public class DSLHolder {
         String line;
         while ((line = br.readLine()) != null) {
             if (line.length() > 0) {
-                throw new RuntimeException("Expected empty line at line " + lineNumber);
+                //throw new RuntimeException("Expected empty line at line " + lineNumber);
+                lineNumber++;
+                continue;
             }
-            String key = br.readLine().trim();
+            String s = br.readLine();
+            if (s == null) {
+                break;
+            }
+            String key = s.trim();
             if (key.length() == 0) {
-                throw new RuntimeException("Key length must not be empty " + (lineNumber + 1));
+               // throw new RuntimeException("Key length must not be empty " + (lineNumber + 1));
+                continue;
             }
             String pinyin = br.readLine().trim();
             if (pinyin.length() == 0) {
-                throw new RuntimeException("Pinyin must not be empty " + (lineNumber + 2));
+                //throw new RuntimeException("Pinyin must not be empty " + (lineNumber + 2));
+                continue;
             }
             String meaning = br.readLine().trim();
             if (meaning.length() == 0) {
-                throw new RuntimeException("Meaning must not be empty " + (lineNumber + 3));
+                //throw new RuntimeException("Meaning must not be empty " + (lineNumber + 3));
+                continue;
             }
             lineNumber += 4;
             res.put(key, new Entry(pinyin, meaning));
